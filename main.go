fc99041fe26873a2064c44562086a02949bf7edc
Pravdin-Main <70532298+Pravdin-Main@users.noreply.github.com>
2022-12-05 23:50:24 +0300

    FLAG lib added

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
+	"flag"
 	"fmt"
-	"os"
+
+	// "os"
 	"strings"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	if scanner.Scan() {
-		if err := scanner.Err(); err != nil {
-			fmt.Sprintln(fmt.Errorf("Some trouble here, somthing like %s", err))
-			return
-		}
-	} else {
-		fmt.Sprintln("Nothing to scann(((")
-		return
-	}
-	fmt.Println(len(strings.Fields(scanner.Text())))
+	// scanner := bufio.NewScanner(os.Stdin)
+	// if scanner.Scan() {
+	// 	if err := scanner.Err(); err != nil {
+	// 		fmt.Sprintln(fmt.Errorf("Some trouble here, somthing like %s", err))
+	// 		return
+	// 	}
+	// } else {
+	// 	fmt.Sprintln("Nothing to scann(((")
+	// 	return
+	// }
+	fmt.Println(len(strings.Fields(strings.Join(flag.Args(), ""))))
+	// fmt.Println(len(strings.Fields(scanner.Text())))
 }
