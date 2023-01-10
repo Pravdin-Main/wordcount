2d50509bd60945661a02653989195e15b733fd75
Pravdin-Main <70532298+Pravdin-Main@users.noreply.github.com>
2023-01-10 21:58:33 +0300

    flag lib used successfully

--- main.go
+++ main.go
@@ -7,17 +7,13 @@ import (
 )
 
 func main() {
-	// var src string
-	// reader := bufio.NewReader(os.Stdin)
-	// src, err := reader.ReadString('\n')
-	// if err != nil {
-	// 	// log.Fatal(err)
-	// }
-	// fmt.Scan(&src)
-	// flag.StringVar(&src, "p", "", "pattern to match against")
-	// // flag.Parse()
-	src := strings.Join(flag.Args(), "")
+	var src string
+	// var src_cache string
 
+	// -------- os lib ------------
+	// src := os.Args
+
+	// ---------- bufio Scanner lib -----------
 	// scanner := bufio.NewScanner(os.Stdin)
 	// if scanner.Scan() {
 	// 	if err := scanner.Err(); err != nil {
@@ -28,20 +24,33 @@ func main() {
 	// 	fmt.Sprintln("Nothing to scann(((")
 	// 	return
 	// }
-	// var count int
-	// for var frase string
-	// for _, err := fmt.Scan(&src); err != nil; {
-	// 	if err == nil {
-	// 		fmt.Println(err)
-	// 	} else {
-	// 		count++
-	// 		_, err = fmt.Scan(&src)
-	// 	}
+
+	// ---------- bufio Reader lib ---------
+	// reader := bufio.NewReader(os.Stdin)
+	// src, err := reader.ReadString('\n')
+	// if err != nil {
+	// 	// log.Fatal(err)
+	// }
+	// fmt.Scan(&src)
+	// flag.StringVar(&src, "p", "", "pattern to match against")
+	// // flag.Parse()
+	// src := strings.Join(flag.Args(), "")
+
+	// ----------- fmt lib ------------
+	// for _, err := fmt.Scanln(&src); err != nil; {
+	// 	_, err = fmt.Scanln(&src_cache)
+	// 	src = src + " " + src_cache
+	// 	src_cache = ""
 	// }
 	// fmt.Fscan(os.Stdin, src)
 	// fmt.Scan(&src)
 	// fmt.Println(fmt.Scan(&src))
-	fmt.Println(len(strings.Fields(src)))
+	// fmt.Println(len(strings.Fields(src)))
+
+	// ----------- flag lib --------------
+	flag.Parse()
+	src = strings.Join(flag.Args(), "")
+
 	// fmt.Println(src)
-	// fmt.Println(len(strings.Fields(scanner.Text())))
+	fmt.Println(len(strings.Fields(src)))
 }
